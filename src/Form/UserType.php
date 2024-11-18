6b72aa9882a37e726a2a2312efe6e5ec6697878e
Alex PNGT <alexandre.panaget@gmail.com>
2024-11-18 12:46:38 +0100

    Création d'entité user et main controller
    Création d'entité user et main controller

---
+++ src/Form/UserType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class UserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname')
+            ->add('lastname')
+            ->add('telephone')
+            ->add('email')
+            ->add('password')
+            ->add('admin')
+            ->add('active')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
