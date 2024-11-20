da586dd8f42e65ec690bdf1b99909cfc426135f1
Alex PNGT <alexandre.panaget@gmail.com>
2024-11-20 14:12:20 +0100

    Added : Pages Campus et Ville
    Ajout des pages Campus et Ville :
    - Il manque le filtre de recherche
    - Style à revoir
    - On peut ajouter / modifier / supprimer directement depuis la table

---
+++ src/Controller/CampusController.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Campus;
+use App\Form\CampusType;
+use App\Form\SearchNameType;
+use App\Repository\CampusRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/campus')]
+class CampusController extends AbstractController
+{
+    #[Route('/', name: 'campus_show', methods: ['GET', 'POST'])]
+    public function show(
+        Request $request,
+        EntityManagerInterface $em,
+        CampusRepository $campusRepository
+    ): Response
+    {
+        // Récupération des campus en BDD
+        $campusLst = $campusRepository->findBy([], ['name' => 'ASC']);
+
+        // Traitement du formulaire d'ajout (Dernière ligne de la table sur la vue)
+        $campus = new Campus();
+        $formCampus = $this->createForm(CampusType::class, $campus);
+        $formCampus->handleRequest($request);
+        if ($formCampus->isSubmitted() && $formCampus->isValid()) {
+            $campus = $formCampus->getData();
+            $em->persist($campus);
+            $em->flush();
+
+            $this->addFlash("success", "Le campus a bien été ajouté");
+            return $this->redirectToRoute('campus_show');
+        }
+        // Autres traitements si nécessaire
+
+        // Affichage de la vue avec les attributs nécessaires
+        return $this->render('campus/show.html.twig', [
+            "campusLst" => $campusLst,
+            "formCampus" => $formCampus,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'campus_edit', requirements: ['id'=>'\d+'], methods: ['GET', 'POST'])]
+    public function edit(
+        Request $request,
+        EntityManagerInterface $em,
+        int $id,
+        CampusRepository $campusRepository
+    ): Response
+    {
+        // Récupération des campus en BDD
+        $campusLst = $campusRepository->findBy([], ['name' => 'ASC']);
+
+        // Récupération du campus à modifier
+        $campus = $campusRepository->find($id);
+        if (!$campus) {
+            throw $this->createNotFoundException('Campus introuvable');
+        }
+
+        // Traitement du formulaire de modification
+        $formCampus = $this->createForm(CampusType::class, $campus);
+        $formCampus->handleRequest($request);
+        if ($formCampus->isSubmitted() && $formCampus->isValid()) {
+            $campus = $formCampus->getData();
+            $em->persist($campus);
+            $em->flush();
+            $this->addFlash("success", "Modification du campus reussie");
+
+            return $this->redirectToRoute('campus_show');
+        }
+
+        return $this->render('campus/edit.html.twig', [
+            'campusToEdit' => $campus,
+            'campusLst' => $campusLst,
+            'formCampus' => $formCampus,
+        ]);
+    }
+
+    #[Route('/{id}/delete', name: 'campus_delete', requirements: ['id'=>'\d+'], methods: ['GET'])]
+    public function delete(
+        Request $request,
+        EntityManagerInterface $em,
+        int $id,
+        CampusRepository $campusRepository
+    ): Response
+    {
+        // Recupération du campus dans la BDD
+        $campus = $campusRepository->find($id);
+        // S'il n'est pas trouvé, on renvoie une exception
+        if (!$campus) {
+            throw $this->createNotFoundException('Campus introuvable');
+        }
+
+        // On vérifie le token CSRF avant de supprimer le campus
+        if ($this->isCsrfTokenValid('delete'.$id, $request->query->get('token'))) {
+            $em->remove($campus);
+            $em->flush();
+            $this->addFlash('success', 'Ce campus a bien été supprimé');
+        }
+        else {
+            $this->addFlash('danger', 'Impossible de supprimer ce campus');
+        }
+
+        return $this->redirectToRoute('campus_show');
+    }
+}
