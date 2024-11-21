8555e563c46a83f91d71ad9260b04a0485784099
Julie35410 <julie.lebigot@hotmail.fr>
2024-11-21 11:53:46 +0100

    page Mon profil
    page Mon profil

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,12 +3,13 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\UserForm;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/user')]
@@ -19,7 +20,7 @@ final class UserController extends AbstractController
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
-        $form = $this->createForm(UserType::class, $user);
+        $form = $this->createForm(UserForm::class, $user);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
@@ -64,11 +65,49 @@ final class UserController extends AbstractController
     #[Route('/{id}', name: 'user_delete', methods: ['POST'])]
     public function delete(Request $request, User $user, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete'.$user->getId(), $request->getPayload()->getString('_token'))) {
+        if ($this->isCsrfTokenValid('delete' . $user->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($user);
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('main_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('main_home', [], Response::HTTP_SEE_OTHER);
     }
-}
+
+    #[Route('/participant/{id}', name: 'user_profil', methods: ['GET', 'POST'])]
+    public function showProfil(int $id,Request $request, UserRepository $userRepository, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $user = $userRepository->find($id); // Récup° du user via son id
+
+        if (!$user) {
+            throw $this->createNotFoundException('Utilisateur non trouvé');
+        }
+
+        // Créer et gérer le formulaire
+        $form = $this->createForm(UserForm::class, $user);
+        $form->handleRequest($request);
+
+        // Si le formulaire est soumis et valide
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var string $plainPassword */
+            $plainPassword = $form->get('plainPassword')->getData();
+
+            // encode the plain password
+            $user->setPassword($userPasswordHasher->hashPassword($user, $plainPassword));
+            $em->flush(); // Enregistrer les modifications
+
+            // Ajouter un message flash de succès
+            $this->addFlash('success', 'Votre profil a été mis à jour.');
+
+            // Rediriger vers la même page pour éviter la resoumission
+            return $this->redirectToRoute('user_profil', ['id' => $id]);
+        }
+
+        // Renvoyer le formulaire à twig
+        return $this->render('user/profil.html.twig', [
+            'user' => $user,
+            'form' => $form->createView(),
+        ]);
+
+
+
+}}
